b1eb3ae0ae11fbf43242b6fbc5a10c8716ee4d92
Laura Illouz <laura.hortense@gmail.com>
2021-07-12 15:55:39 +0200

    Correction fontSize

--- chapitre_01/elsy/src/components/Box.js
+++ chapitre_01/elsy/src/components/Box.js
@@ -1,16 +1,10 @@
 import React from "react";
 
-const style = {
-    fontSize: "100",
-    color: "black"
-}
-
-
 class Box extends React.Component {
     render() {
         return (
             <div className="box col-sm-3 col-6">
-                <span className="material-icons" style={{ color: this.props.color}}>
+                <span className="material-icons" style={{ color: this.props.color, fontSize: 100}}>
                     {this.props.icon}
                 </span>
                 <p>{this.props.value}{this.props.unit}</p>
