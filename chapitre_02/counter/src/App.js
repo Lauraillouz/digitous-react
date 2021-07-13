900cd0a2bd03262fd99182565db902d32b630179
Laura Illouz <laura.hortense@gmail.com>
2021-07-13 14:54:37 +0200

    Exercise2: Compteur V2

--- chapitre_02/counter/src/App.js
+++ chapitre_02/counter/src/App.js
@@ -1,32 +1,16 @@
 import React from "react";
 // Components
-import Button from "./Button";
+import Counter from "./Components/Counter";
 // CSS
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-    }
-  }
 
   render() {
     return (
       <div>
         <h1>Counter</h1>
-        <h2>{this.state.count}</h2>
-        <Button background="green" name="+" onClick={() => {
-          this.setState({
-            count: this.state.count + 1
-          })
-        }} />
-        <Button background="red" name="-" onClick={() => {
-          this.setState({
-            count: this.state.count - 1
-          })
-        }} />
+        <Counter />
       </div>
     )
   }
---
+++ chapitre_02/counter/src/Components/Counter.js
@@ -0,0 +1,34 @@
+import React from "react";
+// Component
+import Button from "./Button";
+
+class Counter extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          count: 0,
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h2>{this.state.count}</h2>
+                <Button background="green" name="+" onClick={() => {
+                    let addFunction = {
+                        count: this.state.count + 1
+                    }
+                    this.setState(addFunction)
+                }} />
+                <Button background="red" name="-" onClick={() => {
+                    let substractFunction = {
+                        count: this.state.count - 1
+                    }
+                    this.setState(substractFunction)
+                }} />
+            </div>
+        )
+    }
+}
+
+export default Counter;
