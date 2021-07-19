5ddd2e5674f1ead077db38a4bb2841f28f2d4fcb
Laura Illouz <laura.hortense@gmail.com>
2021-07-19 15:22:55 +0200

    Exercise09

--- chapitre_01/elsy/src/App.js
+++ chapitre_01/elsy/src/App.js
@@ -28,76 +28,82 @@ class App extends React.Component {
 
 
   onHeartChange = (e) => {
-    this.setState({
-      ...this.state,
+    this.setState((prevState) => ({
+        ...prevState,
         heart: e.target.value
-    })
+    }));
 
-    this.setState((prevState) => {
-      if (this.state.heart >= 120) {
-        return {
-          water: prevState.water + 0.0008
-        }
-      } else {
-          return {
-            water: initialState.water
-          }
-        }
-      })
+    if (this.state.heart >= 120) {
+      this.setState((prevState) => ({
+        ...prevState,
+        water: prevState.water + 0.0008
+      }));
+    } else if (this.state.water > 1.5){
+      this.setState((prevState) => ({
+        ...prevState,
+        water: initialState.water
+      }));
     }
 
-  onStepsChange = (e) => {
-    this.setState({
-      ...this.state,
-      steps: e.target.value
-    })
+  }
 
-    this.setState((prevState) => {
-      if (this.state.steps >= 10000) {
-        return {
-          water: prevState.water + 0.00002
-        }
-      } else {
-        return {
-          water: initialState.water
-        }
-      }
-    })
+  onStepsChange = (e) => {
+    this.setState((prevState) => ({
+      ...prevState,
+      steps: e.target.value
+    }));
+
+    if (this.state.steps >= 10000) {
+      this.setState((prevState) => ({
+        ...prevState,
+        water: prevState.water + 0.00002
+      }));
+    } else if (this.state.water > 1.5){
+      this.setState((prevState) => ({
+        ...prevState,
+        water: initialState.water
+      }));
+    }
   }
 
 
   onTemperatureChange = (e) => {
-    this.setState({
-      ...this.state,
+    this.setState((prevState) => ({
+      ...prevState,
       temperature: e.target.value
-    })
+    }))
 
-    this.setState((prevState) => {
-      if (this.state.temperature >= 20) {
-        return {
-          water: prevState.water + 0.002
-        }
-      } else {
-          return {
-            water: initialState.water
-          };
-        }
-      })  
-    }
-    
+    if (this.state.temperature >= 20) {
+      this.setState((prevState) => ({
+        ...prevState,
+        water: prevState.water + 0.02
+      }));
+    } else if (this.state.water > 1.5){
+      this.setState((prevState) => ({
+        ...prevState,
+        water: initialState.water
+      }));
+    } 
+  }
+
 
   render () {
     return (
       <div className="container-fluid">
         <div className="row">
+
           {/* WATER */}
           <Box icon="local_drink" color="#3A85FF" value={this.state.water} unit="L" /> 
+
           {/* STEPS */}
           <Box icon="directions_walk" color="black" min={stepsMin} max={stepsMax} value={this.state.steps} onChange={this.onStepsChange} unit="steps" />
+
           {/* HEART */}
           <Box icon="favorite" color="red" min={heartMin} max={heartMax} value={this.state.heart} onChange={this.onHeartChange} unit="bpm" />
+
           {/* TEMPERATURE */}
           <Box icon="wb_sunny" color="yellow" min={tempsMin} max={tempsMax} value={this.state.temperature} onChange={this.onTemperatureChange} unit="°C" />
+
         </div>
       </div>
     );
