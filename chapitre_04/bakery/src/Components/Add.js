67427dbb57d4ce0069eb2d8ee06ebcedbe354ca0
Laura Illouz <laura.hortense@gmail.com>
2021-07-21 13:07:44 +0200

    Exercise 4

--- chapitre_04/bakery/src/Components/Add.js
+++ chapitre_04/bakery/src/Components/Add.js
@@ -2,14 +2,40 @@ import React from "react";
 
 class Add extends React.Component {
 
-  render() {
-    return (
-      <div>
-         Add
-      </div>
-    )
+    constructor() {
+        super();
+        this.state = {
+            productName: "",
+            price: 1
+        }
+    }
 
-  }
+    updateProductName = (e) => {
+        this.setState((prevState) => ({
+            ...prevState,
+            productName: e.target.value
+        }));
+    }
+
+    updatePrice = (e) => {
+        this.setState((prevState) => ({
+            ...prevState,
+            price: e.target.value
+        }));
+    }
+
+
+    render() {
+        return (
+        <div>
+            <input type="text" onChange={this.updateProductName}/>
+            <input type="range" min={1} max={10} onChange={this.updatePrice} />
+            <button onClick="">Add</button>
+            
+        </div>
+        )
+
+    }
 }
 
 export default Add;
