ec5af47c7374b5080d2aa2aa7384657239eb2b1b
Laura Illouz <laura.hortense@gmail.com>
2021-08-04 11:06:01 +0200

    03 - Popular

--- chapitre_06/moovice/src/views/Popular.js
+++ chapitre_06/moovice/src/views/Popular.js
@@ -1,6 +1,31 @@
 import React from "react";
 
+const URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=446011991554d3f0bb6bdf42b91d408e";
+
 class Popular extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getMovies();
+    console.log(this.state.movies);
+  }
+
+  getMovies = () => {
+    fetch(`${URL}`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          movies: [data.results],
+        });
+      });
+  };
+
   render() {
     return <div>Popular</div>;
   }
