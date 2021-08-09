e66b5868537816b506f5ead9e3999bb22d4398e0
Laura Illouz <laura.hortense@gmail.com>
2021-08-09 17:23:11 +0200

    updated Popular Battle with hooks

--- chapitre_06/moovice/src/views/PopularBattle.js
+++ chapitre_06/moovice/src/views/PopularBattle.js
@@ -1,85 +1,67 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=446011991554d3f0bb6bdf42b91d408e";
 
-class PopularBattle extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      movies: [],
-      currentBattle: 0,
-    };
-  }
+const PopularBattle = () => {
+  const [movies, setMovies] = useState([]);
+  const [currentBattle, setCurrentBattle] = useState(0);
 
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  getMovies = () => {
-    fetch(`${URL}`)
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({
-          movies: data.results,
+  useEffect(() => {
+    const getMovies = () => {
+      fetch(`${URL}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setMovies(data.results);
         });
-      });
-  };
+    };
+    getMovies();
+  }, []);
 
-  handleClick = (movie) => {
-    if (this.state.currentBattle === this.state.movies.length - 2) {
+  const handleClick = (movie) => {
+    if (currentBattle === movies.length - 2) {
       alert("No more movies to vote for!");
     } else {
-      this.setState((prevState) => ({
-        ...prevState,
-        currentBattle: prevState.currentBattle + 2,
-      }));
+      setCurrentBattle((prevState) => prevState.currentBattle + 2);
     }
-    this.saveMovie(movie);
+    saveMovie(movie);
   };
 
-  saveMovie = (movie) => {
+  const saveMovie = (movie) => {
     let storedMovie = JSON.parse(localStorage.getItem("favorites") || "[]");
     storedMovie.push(movie.id);
     localStorage.setItem("favorites", JSON.stringify(storedMovie));
   };
 
-  render() {
-    return (
-      <div>
-        <h1 className="m-4">Popular Battle</h1>
-        <div className="d-flex flex-wrap justify-content-center">
-          {this.state.movies
-            .slice(this.state.currentBattle, this.state.currentBattle + 2)
-            .map((movie) => {
-              return (
-                <div
-                  className="m-4"
-                  onClick={() => this.handleClick(movie)}
-                  key={movie.id}
-                >
-                  <div
-                    className="card"
-                    style={{ width: "18rem", height: "50rem" }}
-                  >
-                    <img
-                      className="card-img-top"
-                      src={`http://image.tmdb.org/t/p/w300${movie.poster_path}`}
-                      alt={movie.title}
-                    ></img>
-                    <div className="card-body">
-                      <p style={{ fontWeight: "bold" }}>{movie.title}</p>
-                      <p>{movie.release_date}</p>
-                      <p style={{ fontSize: "0.75rem" }}>{movie.overview}</p>
-                    </div>
-                  </div>
+  return (
+    <div>
+      <h1 className="m-4">Popular Battle</h1>
+      <div className="d-flex flex-wrap justify-content-center">
+        {movies.slice(currentBattle, currentBattle + 2).map((movie) => {
+          return (
+            <div
+              className="m-4"
+              onClick={() => handleClick(movie)}
+              key={movie.id}
+            >
+              <div className="card" style={{ width: "18rem", height: "50rem" }}>
+                <img
+                  className="card-img-top"
+                  src={`http://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                  alt={movie.title}
+                ></img>
+                <div className="card-body">
+                  <p style={{ fontWeight: "bold" }}>{movie.title}</p>
+                  <p>{movie.release_date}</p>
+                  <p style={{ fontSize: "0.75rem" }}>{movie.overview}</p>
                 </div>
-              );
-            })}
-        </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default PopularBattle;
