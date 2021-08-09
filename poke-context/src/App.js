33c60c84c2453608f3ef644776f00ad7873076d4
Laura Illouz <laura.hortense@gmail.com>
2021-08-09 13:44:19 +0200

    Set up Pages

--- poke-context/src/App.js
+++ poke-context/src/App.js
@@ -1,7 +1,33 @@
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+// Components
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Error404 from "./components/Error404";
+
+//CSS
 import "./App.css";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="*" component={Error404} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
