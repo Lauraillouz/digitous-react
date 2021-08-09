79163cf3129b10656868e20de4f43419b3f857f1
Laura Illouz <laura.hortense@gmail.com>
2021-08-09 14:00:48 +0200

    Login form created

--- poke-context/src/components/Login.js
+++ poke-context/src/components/Login.js
@@ -1,5 +1,31 @@
+import { useForm } from "react-hook-form";
+
 const Login = () => {
-  return <div>Login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("username")} type="text" required maxLength={15} />
+        {errors.username && <span>Username is required</span>}
+        <input
+          {...register("password")}
+          type="password"
+          required
+          maxLength={6}
+        />
+        {errors.password && <span>Password is required</span>}
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
