8a1e811925bc126df06b961197a92bf88d389153
Laura Illouz <laura.hortense@gmail.com>
2021-08-09 17:15:55 +0200

    updated Card with hooks

--- chapitre_06/moovice/src/components/Card.js
+++ chapitre_06/moovice/src/components/Card.js
@@ -1,30 +1,26 @@
-import React from "react";
-
-class Card extends React.Component {
-  render() {
-    return (
-      <div className="d-flex flex-wrap justify-content-center">
-        {this.props.movies.map((movie) => {
-          return (
-            <div className="m-4" key={movie.id}>
-              <div className="card" style={{ width: "18rem", height: "50rem" }}>
-                <img
-                  className="card-img-top"
-                  src={`http://image.tmdb.org/t/p/w300${movie.poster_path}`}
-                  alt={movie.title}
-                ></img>
-                <div className="card-body">
-                  <p style={{ fontWeight: "bold" }}>{movie.title}</p>
-                  <p>{movie.release_date}</p>
-                  <p style={{ fontSize: "0.75rem" }}>{movie.overview}</p>
-                </div>
+const Card = (props) => {
+  return (
+    <div className="d-flex flex-wrap justify-content-center">
+      {props.movies.map((movie) => {
+        return (
+          <div className="m-4" key={movie.id}>
+            <div className="card" style={{ width: "18rem", height: "50rem" }}>
+              <img
+                className="card-img-top"
+                src={`http://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                alt={movie.title}
+              ></img>
+              <div className="card-body">
+                <p style={{ fontWeight: "bold" }}>{movie.title}</p>
+                <p>{movie.release_date}</p>
+                <p style={{ fontSize: "0.75rem" }}>{movie.overview}</p>
               </div>
             </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Card;
--- chapitre_06/moovice/src/components/Navbar.js
+++ chapitre_06/moovice/src/components/Navbar.js
@@ -1,57 +1,54 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/weekly">
-                  Weekly
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  aria-current="page"
-                  to="/weekly-battle"
-                >
-                  Weekly Battle
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/popular">
-                  Popular
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  aria-current="page"
-                  to="/popular-battle"
-                >
-                  Popular Battle
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/favorites">
-                  Favorites
-                </Link>
-              </li>
-            </ul>
-          </div>
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/weekly">
+                Weekly
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                aria-current="page"
+                to="/weekly-battle"
+              >
+                Weekly Battle
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/popular">
+                Popular
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                aria-current="page"
+                to="/popular-battle"
+              >
+                Popular Battle
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/favorites">
+                Favorites
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
