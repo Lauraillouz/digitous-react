693fc9852ebc7a1b4db666c35d864a68c6e74b38
Laura Illouz <laura.hortense@gmail.com>
2021-08-11 12:29:12 +0200

    conditional render in Home to show data only when logged in

--- poke-context/src/components/Home.js
+++ poke-context/src/components/Home.js
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "../App";
 
 function randomNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
 const Home = () => {
+  const isLoggedState = useContext(UserContext);
+
   const [pokemon, setPokemon] = useState({});
   /*   const [newPokemon, setNewPokemon] = useState(); */
 
@@ -23,23 +26,29 @@ const Home = () => {
   return (
     <div className="bg-dark text-light text-center container-fluid p-5">
       <h2>Home</h2>
-      <button className="btn btn-info ps-5 pe-5" onClick={getPokemon}>
-        Get a Pokemon
-      </button>
-      <ul className="list-group list-group-flush ">
-        <li className="list-group-item bg-dark">
-          <p className="mt-5 text-light">Name: {pokemon.name}</p>
-        </li>
-        <li className="list-group-item bg-dark">
-          <p className="text-light">Height: {pokemon.height}</p>
-        </li>
-        <li className="list-group-item bg-dark">
-          <p className="text-light">Weight: {pokemon.weight}</p>
-        </li>
-        <li className="list-group-item bg-dark">
-          <p className="mb-5 text-light">Type: {pokemon.type}</p>
-        </li>
-      </ul>
+      {isLoggedState.isLogged ? (
+        <div>
+          <button className="btn btn-info ps-5 pe-5" onClick={getPokemon}>
+            Get a Pokemon
+          </button>
+          <ul className="list-group list-group-flush ">
+            <li className="list-group-item bg-dark">
+              <p className="mt-5 text-light">Name: {pokemon.name}</p>
+            </li>
+            <li className="list-group-item bg-dark">
+              <p className="text-light">Height: {pokemon.height}</p>
+            </li>
+            <li className="list-group-item bg-dark">
+              <p className="text-light">Weight: {pokemon.weight}</p>
+            </li>
+            <li className="list-group-item bg-dark">
+              <p className="mb-5 text-light">Type: {pokemon.type}</p>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        "You need to logged in."
+      )}
     </div>
   );
 };
